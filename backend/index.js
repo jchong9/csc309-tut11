123f4483db52a1c93aaa26b43439f028f23c122f
Jeremy Chong <chong.jeremy@hotmail.com>
2025-11-18 00:56:01 -0500

    remove temp logging

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,6 @@ app.use(cors({
     credentials: true,
 }));
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log("CORS Origin Attempt:", req.headers.origin);
-  next();
-});
 app.use('', routes);
 
 export default app;
